2fa36910f6e9aa99bb9748e37357eaf84fbfe484
Debbzz <moyinoluwaezekiel72@gmail.com>
2026-05-10 06:51:04 +0100

    Preserve JSON headers in API helper

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,14 @@
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000';
 
 export async function apiRequest(path, options = {}) {
+  const { headers = {}, ...restOptions } = options;
+
   const response = await fetch(`${API_BASE_URL}${path}`, {
+    ...restOptions,
     headers: {
       'Content-Type': 'application/json',
-      ...(options.headers || {}),
+      ...headers,
     },
-    ...options,
   });
 
   const data = await response.json().catch(() => ({}));
